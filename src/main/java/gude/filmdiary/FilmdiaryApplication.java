d3926c70368ef288ae32b5a1d71864fa75c49ea1
davidcarlin <david.carlin@barclays.com>
2023-05-24 17:40:18 +0100

    initial commit

---
+++ src/main/java/gude/filmdiary/FilmdiaryApplication.java
@@ -0,0 +1,13 @@
+package gude.filmdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmdiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gude/filmdiary/FilmdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package gude.filmdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
